db44422f0c5c4523607a0f336c71a079b245631c
napatsorn kamsaard <59030174@kmitl.ac.th>
2017-08-31 14:32:38 +0700

    learn to database

--- Engedu2/Program.cs
+++ Engedu2/Program.cs
@@ -10,14 +10,15 @@ namespace Engedu2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year 255x seamster 1 , I am Engedu freshman.");
-            Console.WriteLine("Year 255x seamster 2 , I learn to program in c sharp.");
-            Console.WriteLine("Year 2560 seamster 1 , I start my programmer life.");
-            Console.WriteLine("Year 2560 seamster 2 , I learn to GUI.");
-            Console.WriteLine("Year 2561 seamster 1 , I start to coach.");
-            Console.WriteLine("Year 2561 seamster 2 , I learn to sofware.");
-            Console.WriteLine("Year 2562 seamster 1 , I make a project.");
-
+            Console.WriteLine("Year 255x semester 1 , I am Engedu freshman.");
+            Console.WriteLine("Year 255x semester 2 , I learn to program in c sharp.");
+            Console.WriteLine("Year 2560 semester 1 , I start my programmer life.");
+            Console.WriteLine("Year 2560 semester 2 , I learn to GUI.");
+            Console.WriteLine("Year 2561 semester 1 , I start to coach.");
+            Console.WriteLine("Year 2561 semester 2 , I learn to sofware.");
+            Console.WriteLine("Year 2562 semester 1 , I make a project.");
+            Console.WriteLine("Year 2562 semester 2 , I learn to database.")
+           
 
         }
     }
